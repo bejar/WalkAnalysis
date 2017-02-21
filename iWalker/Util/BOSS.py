60d17a209912c88b8ca3ad418cb41d76af3a1fa4
javier.bejar <bejar@cs.upc.edu>
2017-02-21 15:01:35 +0100

    Implementing BOSS

--- iWalker/Util/BOSS.py
+++ iWalker/Util/BOSS.py
@@ -18,6 +18,10 @@ BOSS
 """
 
 from iWalker.Util import mft
+import numpy as np
+from kemlglearn.preprocessing import Discretizer
+import seaborn as sn
+from collections import Counter
 
 __author__ = 'bejar'
 
@@ -33,12 +37,13 @@ class Boss():
         :param sampling:
         """
         self.series = lseries
-        self.samplig = sampling
-        self. coefs = []
+        self.sampling = sampling
+        self.coefs = []
 
-    def compute(self, ncoef, wsize):
+    def discretization_intervals(self, ncoef, wsize, vsize):
         """
-        Computes the BOSS codes for the signals, the word length is 2*ncoefs (real and imaginary part)
+        Computes the BOSS discretization for the signals, the word length is 2*ncoefs (real and imaginary part) except
+        is there are coefficients that are zero
 
         :param ncoef:
         :param wsize:
@@ -46,9 +51,70 @@ class Boss():
         """
 
         for s in self.series:
-            self.coefs.append(mft(s, self.sampling, ncoef, wsize))
+            coefs = mft(s, self.sampling, ncoef, wsize)
+            lcoefs = []
+            for i in range(coefs.shape[1]):
+                lcoefs.append(coefs[:,i].real)
+                lcoefs.append(coefs[:,i].imag)
+
+            self.coefs.append(np.stack(lcoefs, axis=-1))
+
+        X = np.concatenate(self.coefs)
+
+        self.disc = Discretizer(method='frequency', bins=vsize)
+        self.disc.fit(X)
+
+    def discretize(self):
+        """
+        Computes the words for each time series
+        :param series:
+        :return:
+        """
+        vocabulary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        def word(vec):
+            """
+
+            :param v:
+            :return:
+            """
+            w = ''
+            for v in vec:
+                w += vocabulary[int(v)]
+            return w
+
+        for c in self.coefs:
+            sdisc = self.disc.transform(c, copy=True).real
+            prevw = word(sdisc[0])
+            lvoc = [prevw]
+            for i in range(1,sdisc.shape[0]):
+                nword = word(sdisc[i])
+                if nword != prevw:
+                    lvoc.append(nword)
+            print(Counter(lvoc))
 
 
+if __name__ == '__main__':
+    from iWalker.Data import User, Exercise, Exercises, Pacientes, Trajectory
+    from iWalker.Util.Misc import show_list_signals
+    p = Pacientes()
+    e = Exercises()
+    p.from_db(pilot='NOGA')
+    e.from_db(pilot='NOGA')
+    e.delete_patients(['FSL30'])
 
+    lseries = []
+
+    i = 0
+    for ex in e.iterator():
+        if i < 3:
+            lseries.append(ex.get_forces()[:, 0])
+        else:
+            break
+        i += 1
+
+    boss = Boss(lseries, 10)
+
+    boss.discretization_intervals(2, 32, 3)
+    boss.discretize()
 
 
